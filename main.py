006905fd79d713d50f0b15a0fa1a607eb2230042
parad1se_jp <1294807419@qq.com>
2022-07-22 03:34:11 +0900

    use matrix computation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly
 import plotly.express as px
+import numpy as np
 
 
 def oscillate_range(stop):
@@ -84,6 +85,45 @@ def plot_4d_to_3d():
     fig.write_html('./4dto3d.html')
 
 
+def matrix_plot_3d_to_2d():
+    X = np.arange(-20, 20)
+    Y = np.arange(-20, 20)
+    Z = np.arange(-20, 20)
+    x, y, z = np.meshgrid(X, Y, Z)
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    r_idx = np.where(r <= 15)
+    df = pd.DataFrame({'x': X[r_idx[0]],
+                       'y': Y[r_idx[1]],
+                       'z': Z[r_idx[2]]})
+    fig = px.scatter(data_frame=df, x='x', y='y', color='z', color_continuous_scale=plotly.colors.sequential.Viridis)
+    fig.update_yaxes(
+        scaleanchor="x",
+        scaleratio=1,
+    )
+    fig.show()
+    fig.write_html('./3dto2d_matrix.html')
+
+
+def matrix_plot_4d_to_3d():
+    X = np.arange(-20, 20)
+    Y = np.arange(-20, 20)
+    Z = np.arange(-20, 20)
+    W = np.arange(0, 40)
+    x, y, z, w = np.meshgrid(X, Y, Z, W)
+    r = np.sqrt((x - 0) ** 2 + (y - 0) ** 2 + (z - 0) ** 2 + (w - 20) ** 2)
+    r_idx = np.where(r <= 15)
+    df4d = pd.DataFrame({'x': X[r_idx[0]],
+                         'y': Y[r_idx[1]],
+                         'z': Z[r_idx[2]],
+                         'w': W[r_idx[3]]})
+    fig = px.scatter_3d(df4d, x='x', y='y', z='z', color='w', color_continuous_scale=plotly.colors.sequential.Viridis)
+    fig.update_traces(marker=dict(size=2))
+    fig.show()
+    fig.write_html('./3dto2d_matrix.html')
+
+
 if __name__ == '__main__':
-    plot_3d_to_2d()
-    plot_4d_to_3d()
+    # plot_3d_to_2d()
+    # plot_4d_to_3d()
+    # matrix_plot_3d_to_2d()
+    matrix_plot_4d_to_3d()
