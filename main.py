99331ed3fc8ea4659e05ce016f9b0b40172af6da
parad1se_jp <1294807419@qq.com>
2022-09-02 03:14:43 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import timeit
 import pandas as pd
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
+from tqdm import tqdm
 
 
 def oscillate_range(stop):
@@ -29,7 +32,7 @@ def plot_3d_to_2d():
                        'z': [],
                        'r': []})
 
-    for r in range(21):
+    for r in tqdm(range(21)):
         for x in oscillate_range(40):
             for y in oscillate_range(40):
                 if x ** 2 + y ** 2 > r ** 2:
@@ -45,7 +48,8 @@ def plot_3d_to_2d():
                                 'r': r}
                         df = pd.concat([df, pd.DataFrame(data, index=[0])])
 
-    fig = px.scatter(data_frame=df, x='x', y='y', color='z', color_continuous_scale=plotly.colors.sequential.Viridis)
+    fig = px.scatter(data_frame=df, x='x', y='y', color='z',
+                     color_continuous_scale=plotly.colors.sequential.Viridis)
     fig.update_yaxes(
         scaleanchor="x",
         scaleratio=1,
@@ -78,8 +82,10 @@ def plot_4d_to_3d():
                                     'z': z,
                                     'w': w,
                                     'r': r}
-                            df4d = pd.concat([df4d, pd.DataFrame(data, index=[0])])
-    fig = px.scatter_3d(df4d, x='x', y='y', z='z', color='w', color_continuous_scale=plotly.colors.sequential.Viridis)
+                            df4d = pd.concat(
+                                [df4d, pd.DataFrame(data, index=[0])])
+    fig = px.scatter_3d(df4d, x='x', y='y', z='z', color='w',
+                        color_continuous_scale=plotly.colors.sequential.Viridis)
     fig.update_traces(marker=dict(size=2))
     fig.show()
     fig.write_html('./4dto3d.html')
@@ -95,35 +101,40 @@ def matrix_plot_3d_to_2d():
     df = pd.DataFrame({'x': X[r_idx[0]],
                        'y': Y[r_idx[1]],
                        'z': Z[r_idx[2]]})
-    fig = px.scatter(data_frame=df, x='x', y='y', color='z', color_continuous_scale=plotly.colors.sequential.Viridis)
+    fig = px.scatter(data_frame=df, x='x', y='y', color='z',
+                     color_continuous_scale=plotly.colors.sequential.Viridis)
     fig.update_yaxes(
         scaleanchor="x",
         scaleratio=1,
     )
     fig.show()
-    fig.write_html('./3dto2d_matrix.html', include_plotlyjs='cdn', full_html=False)
+    fig.write_html('./3dto2d_matrix.html',
+                   include_plotlyjs='cdn', full_html=False)
 
 
 def matrix_plot_4d_to_3d():
-    X = np.arange(-20, 20)
-    Y = np.arange(-20, 20)
-    Z = np.arange(-20, 20)
-    W = np.arange(0, 40)
+    X = np.arange(-5, 5)
+    Y = np.arange(-5, 5)
+    Z = np.arange(-5, 5)
+    W = np.arange(0, 10)
     x, y, z, w = np.meshgrid(X, Y, Z, W)
-    r = np.sqrt((x - 0) ** 2 + (y - 0) ** 2 + (z - 0) ** 2 + (w - 20) ** 2)
-    r_idx = np.where(r <= 15)
+    r = np.sqrt((x - 0) ** 2 + (y - 0) ** 2 + (z - 0) ** 2 + (w - 5) ** 2)
+    r_idx = np.where(r <= 5)
     df4d = pd.DataFrame({'x': X[r_idx[0]],
                          'y': Y[r_idx[1]],
                          'z': Z[r_idx[2]],
                          'w': W[r_idx[3]]})
-    fig = px.scatter_3d(df4d, x='x', y='y', z='z', color='w', color_continuous_scale=plotly.colors.sequential.Viridis)
+    fig = px.scatter_3d(df4d, x='x', y='y', z='z', color='w',
+                        color_continuous_scale=plotly.colors.sequential.Viridis)
     fig.update_traces(marker=dict(size=2))
     fig.show()
-    fig.write_html('./4dto3d_matrix.html', include_plotlyjs='cdn', full_html=False)
+    fig.write_html('./4dto3d_matrix.html',
+                   include_plotlyjs='cdn', full_html=False)
 
 
 if __name__ == '__main__':
     # plot_3d_to_2d()
     # plot_4d_to_3d()
-    matrix_plot_3d_to_2d()
-    matrix_plot_4d_to_3d()
+    # matrix_plot_3d_to_2d()
+    print(timeit.timeit(stmt=matrix_plot_4d_to_3d, number=1))
+    # matrix_plot_4d_to_3d()
